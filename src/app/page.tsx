0cabb741b38000ab64f2fea23349691e6435549d
susil dahal <susildahal234@gmail.com>
2025-09-19 19:17:13 +0545

    fdsdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const page = () => {
   return (
     <div className='  flex  justify-end items-end mt-4 pr-3'>
-      <Link href='/login'> <Button> Login </Button></Link> 
+      <Link href='/login'> <Button>  click to Login </Button></Link> 
       
     </div>
   )
